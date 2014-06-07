9785b99ca678d1fcb64bade62116541c5b022788
Janice Smith <janice.k.n.smith@gmail.com>
2014-06-07 19:42:57 -0400

    set up fixtures, a route, controller and template for Messages

--- app/models/message.js
+++ app/models/message.js
@@ -1,8 +1,24 @@
-attr = DS.attr
+var attr = DS.attr;
 
-export default DS.Model.extend({
+var Message = DS.Model.extend({
   title: attr('string'),
   contentOriginal: attr('string'),
   contentEncoded: attr('string'),
   author: attr('string')
 });
+
+Message.FIXTURES = [{
+  id: 1,
+  title: 'this is a title',
+  contentOriginal: 'this is the content',
+  contentEncoded: 'encoded?',
+  author: 'Janice the Great and Powerful'
+},{
+  id: 2,
+  title: 'this is another title',
+  contentOriginal: 'this is the content again',
+  contentEncoded: 'encoded? nope',
+  author: 'Janice the Great and Powerful'
+}];
+
+export default Message;
